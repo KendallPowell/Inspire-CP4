66ac5717a9b4522fc1525005538465678c669150
KendallPowell <kendalpowel@hotmail.com>
2022-11-22 10:55:43 -0700

    ?

--- app/AppState.js
+++ app/AppState.js
@@ -5,7 +5,7 @@ import { loadState } from "./Utils/Store.js"
 
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', [Value])
+  // values = loadState('values', [Value])
 
 
 
--- app/Services/ImagesService.js
+++ app/Services/ImagesService.js
@@ -1,5 +1,8 @@
 
 
+export const imagesApi = new axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/images'
+})
 
 class ImagesController {
 
